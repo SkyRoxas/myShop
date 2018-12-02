c97c9c9384af13e2f5b5d6dce90788014fbfea34
skyroxas <skyroxas1521@gmail.com>
2018-12-02 15:17:16 +0800

    feat: 完成 handbar 模板教學

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const expressHbs = require('express-handlebars')
 
 const adminData = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
@@ -10,7 +11,8 @@ const rootDir = require('./util/path')
 
 const app = express()
 
-app.set('view engine', 'pug')
+app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}))
+app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({extended: false}))
--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,16 @@ const router = express.Router()
 router.get('/', (req, res) => {
     const {products} = adminData
     console.log('products :', products);
-    res.render('shop', {prods: products, pageTitle: 'Shop', path: "/"})
+    res.render('shop', 
+        {
+            prods: products,
+            pageTitle: 'Shop',
+            path: "/",
+            hasProducts: products.length > 0,
+            activeShop: true,
+            productCss: true,
+        }
+    )
 })
 
 module.exports = router
