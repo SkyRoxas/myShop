86a0fe9e254786889d51083ab360ba99c55db2e6
skyroxas <skyroxas1521@gmail.com>
2018-12-02 15:52:20 +0800

    feat: 完成 ejs 的模板教學

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const rootDir = require('./util/path')
 
 const app = express()
 
-app.engine('hbs', expressHbs({layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs'}))
-app.set('view engine', 'hbs')
+app.set('view engine', 'ejs')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({extended: false}))
